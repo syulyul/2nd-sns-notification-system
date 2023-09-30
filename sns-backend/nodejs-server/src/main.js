0d7ea1fd825d5c1a10e9f3438da899edf6eab6cc
pangorithm <phs559800@gmail.com>
2023-09-30 20:24:41 +0900

    feat: nodeJS 서버 초기 세팅
    채팅 서버 개발을 위한 nodeJS 서버 초기 세팅
    
    9월30일 task #4

---
+++ sns-backend/nodejs-server/src/main.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+import express from "express";
+import morgan from "morgan";
+import path from "path";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import api from "./api";
+import mongodbConnect from "./schemas";
+
+const { PORT, NODE_ENV, COOKIE_SECRET } = process.env;
+
+const app = express();
+app.set("port", PORT || 4000);
+// mongodbConnect(); // 몽고디비 미세팅 상태
+
+if (NODE_ENV === "production") {
+  app.use(morgan("combined"));
+} else {
+  app.use(morgan("dev"));
+}
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+app.use("/api", api);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+});
+
+const server = app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
+});
---
+++ sns-backend/nodejs-server/src/schemas/index.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const mongodbConnect = () => {
+  const { NODE_ENV, MONGO_URI, MONGODB_USER, MONGODB_PASS } = process.env;
+
+  console.log("MONGODB연결 시작");
+  const MONGO_URL = `mongodb://${MONGODB_USER}:${MONGODB_PASS}@${MONGO_URI}`;
+  if (NODE_ENV !== "production") {
+    mongoose.set("debug", true);
+  }
+  mongoose
+    .connect(MONGO_URL, {
+      dbName: "",
+    })
+    .then(() => {
+      console.log("MongoDB 연결성공");
+    })
+    .catch((err) => {
+      console.error("@mongoDB 연결 에러발생@", err);
+    });
+};
+
+mongoose.connection.on("error", (error) => {
+  console.error("몽고디비 연결 에러", error);
+});
+mongoose.connection.on("disconnected", () => {
+  console.error("몽고디비 연결이 끊겼습니다. 연결을 재시도합니다.");
+  mongodbConnect();
+});
+
+export default mongodbConnect;
