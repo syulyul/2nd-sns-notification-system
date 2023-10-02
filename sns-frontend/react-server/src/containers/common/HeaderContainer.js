1f3a6b4d6d45e2110c601de4b3b3e7a0e75e8a9b
pangorithm <phs559800@gmail.com>
2023-10-02 13:09:34 +0900

    feat: create common & header components

---
+++ sns-frontend/react-server/src/containers/common/HeaderContainer.js
@@ -0,0 +1,8 @@
+import { useDispatch, useSelector } from 'react-redux';
+import Header from '../../components/common/Header';
+
+const HeaderContainer = () => {
+  return <Header />;
+};
+
+export default HeaderContainer;
