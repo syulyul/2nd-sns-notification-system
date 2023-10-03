885ab0e92bec7ad697cf9047e7a1f34500a36b82
pangorithm <phs559800@gmail.com>
2023-10-03 15:14:12 +0900

    refactor: redis service 변경
    redis service를 통해 redis 오퍼레이션을 반환 받아 redis 자료구조를 다룰 수 있도록 변경
    
    redis 연동 #10

--- sns-backend/snsapp-2nd/spring-server/src/main/java/bitcamp/myapp/service/RedisService.java
+++ sns-backend/snsapp-2nd/spring-server/src/main/java/bitcamp/myapp/service/RedisService.java
@@ -1,12 +1,11 @@
 package bitcamp.myapp.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,54 +18,25 @@ public class RedisService {
     this.redisTemplate = redisTemplate;
   }
 
-  // string (opsForValue)
-  public void setStringOps(String key, String value, long ttl, TimeUnit unit) {
-    redisTemplate.opsForValue().set(key, value, ttl, unit);
+  public ValueOperations getValuleOps() {
+    return redisTemplate.opsForValue();
   }
 
-  public String getStringOps(String key) {
-    return redisTemplate.opsForValue().get(key);
+  public ListOperations getListOps() {
+    return redisTemplate.opsForList();
   }
 
-  // list (opsForList)
-  public void setListOps(String key, List<String> values) {
-    redisTemplate.opsForList().rightPushAll(key, values);
+  public SetOperations getSetOps() {
+    return redisTemplate.opsForSet();
   }
 
-  public List<String> getListOps(String key) {
-    Long len = redisTemplate.opsForList().size(key);
-    return len == 0 ? new ArrayList<>() : redisTemplate.opsForList().range(key, 0, len - 1);
+  public ZSetOperations getSortedSetOps() {
+    return redisTemplate.opsForZSet();
   }
 
-  // hash (opsForHash)
-  public void setHashOps(String key, HashMap<String, String> value) {
-    redisTemplate.opsForHash().putAll(key, value);
+  public HashOperations getHashOps() {
+    return redisTemplate.opsForHash();
   }
 
-  public String getHashOps(String key, String hashKey) {
-    return redisTemplate.opsForHash().hasKey(key, hashKey) ? (String) redisTemplate.opsForHash()
-        .get(key, hashKey) : "";
-  }
-
-  // set (opsForSet)
-  public void setSetOps(String key, String... values) {
-    redisTemplate.opsForSet().add(key, values);
-  }
-
-  public Set<String> getSetOps(String key) {
-    return redisTemplate.opsForSet().members(key);
-  }
-
-  // sorted set (opsForZSet)
-//  public void setSortedSetOps(String key, List<Struct.SortedSet> values) {
-//    for (Struct.SortedSet v : values) {
-//      redisTemplate.opsForZSet().add(key, v.getValue(), v.getScore());
-//    }
-//  }
-
-  public Set getSortedSetOps(String key) {
-    Long len = redisTemplate.opsForZSet().size(key);
-    return len == 0 ? new HashSet<String>() : redisTemplate.opsForZSet().range(key, 0, len - 1);
-  }
 
 }
