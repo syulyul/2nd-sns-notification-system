b8c66405241dd7b491f4d2bd09d40b3a52d38cd4
lorajung <lorajung@naver.com>
2023-10-05 19:43:31 +0900

    feat : BoardFormComponent, BoardFormContainer 작성

---
+++ sns-frontend/react-server/src/pages/BoardFormPage.js
@@ -0,0 +1,7 @@
+import BoardFormContainer from '../containers/board/BoardFormContainer';
+
+const BoardFormPage = () => {
+  return <BoardFormContainer />;
+};
+
+export default BoardFormPage;
--- sns-frontend/react-server/src/pages/WriteFormPage.js
+++
@@ -1,7 +0,0 @@
-import WriteFormContainer from '../containers/board/WriteFormContainer';
-
-const WriteFormPage = () => {
-  return <WriteFormContainer />;
-};
-
-export default WriteFormPage;
