d9e2dfc016c1d8d81c68f9a2d48316b2cb253cab
Jung Yurn Soo <lorajung@naver.com>
2023-10-05 21:48:11 +0900

    feat : NotificationListComponent, NotificationListContainer 작성

--- sns-frontend/react-server/src/containers/notification/NotificationListContainer.js
+++ sns-frontend/react-server/src/containers/notification/NotificationListContainer.js
@@ -3,17 +3,12 @@ import React, { useState, useEffect } from 'react';
 import NotificationListComponent from '../../components/notification/NotificationListComponent';
 
 const NotificationListContainer = () => {
-  const [notifications, setNotifications] = useState([]);
+  const [notificationData, setNotificationData] = useState([
+  { id: 1, content: "첫 번째 알림", notiState: 0, url: "#" },
+  { id: 2, content: "두 번째 알림", notiState: 1, url: "#" }
+  ]);
 
-  useEffect(() => {
-    const mockData = [
-      { id: 1, content: "첫 번째 알림", notiState: 0, url: "#" },
-      { id: 2, content: "두 번째 알림", notiState: 1, url: "#" }
-    ];
-    setNotifications(mockData);
-  }, []);
-
-  return <NotificationListComponent notifications={notifications} />;
+  return <NotificationListComponent notificationData={notificationData} />;
 };
 
 export default NotificationListContainer;
