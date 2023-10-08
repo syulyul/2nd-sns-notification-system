dd4b571f802418c3886b8c057bc23b45bf473814
pangorithm <phs559800@gmail.com>
2023-10-09 01:38:06 +0900

    feat: node server notilog service create

--- sns-backend/nodejs-server/src/api/notification/index.js
+++ sns-backend/nodejs-server/src/api/notification/index.js
@@ -1,7 +1,12 @@
-import express from "express";
+import express from 'express';
+import * as notiCtrl from './notification.ctrl';
 
 const notification = express.Router();
 
-notification.get("/");
+notification.post('/add', notiCtrl.addLog);
+notification.post('/notReadNotiCount', notiCtrl.notReadNotiCount);
+notification.post('/list', notiCtrl.listNotiLog);
+notification.post('/updateState', notiCtrl.updateNotiState);
+notification.post('/updateAllState', notiCtrl.updateAllNotiState);
 
 export default notification;
