f951f0895794be88fb116bc5609be128e2bea4a8
pangorithm <phs559800@gmail.com>
2023-10-08 10:17:59 +0900

    refactor: proxy 설정 방식 변경
    package.json을 이용한 개발용 프록시 설정  -> http-proxy-middleware을 이용한 프록시 설정

--- sns-frontend/react-server/src/lib/api/client.js
+++ sns-frontend/react-server/src/lib/api/client.js
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
-const cookies = document.cookie.split(";");
+const cookies = document.cookie.split(';');
 const csrfTokenCookie = cookies.find((cookie) =>
-  cookie.trim().startsWith("csrfToken=")
+  cookie.trim().startsWith('csrfToken=')
 );
-const csrfToken = csrfTokenCookie ? csrfTokenCookie.split("=")[1] : null;
+const csrfToken = csrfTokenCookie ? csrfTokenCookie.split('=')[1] : null;
 
 const client = axios.create();
-client.defaults.headers.common["X-CSRF-Token"] = csrfToken;
+client.defaults.headers.common['X-CSRF-Token'] = csrfToken;
 
 export default client;
---
+++ sns-frontend/react-server/src/setupProxy.js
@@ -0,0 +1,20 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware(['/spring', '/img'], {
+      target: 'http://localhost:4000/',
+      changeOrigin: true,
+    })
+  );
+
+  // app.use(
+  //   createProxyMiddleware(['/api', '/img', '/sse', '/socket.io'], {
+  //     target: 'http://localhost:3001/',
+  //     changeOrigin: true,
+  //     ws: true,
+  //     router: {
+  //       '/socket.io': 'ws://localhost:3001/',
+  //     },
+  //   })
+  // );
+};
