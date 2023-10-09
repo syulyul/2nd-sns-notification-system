1be572dfed3fe579791ac68430251054929fbcf7
wjswnsgh98 <jeonjunho777@naver.com>
2023-10-09 13:12:36 +0900

    마이페이지 상세보기 페이지 스프링서버랑 리액트 서버 연동

--- sns-frontend/react-server/src/modules/index.js
+++ sns-frontend/react-server/src/modules/index.js
@@ -3,15 +3,17 @@ import { all } from 'redux-saga/effects';
 import loading from './loading';
 import object, { objectSaga } from './exampleAction';
 import auth, { authSaga } from './auth';
+import myPage, {myPageSaga} from "./myPage";
 
 const rootReducer = combineReducers({
   loading,
   object,
   auth,
+  myPage,
 });
 
 export function* rootSaga() {
-  yield all([objectSaga(), authSaga()]);
+  yield all([objectSaga(), authSaga(), myPageSaga()]);
 }
 
 export default rootReducer;
--- sns-frontend/react-server/src/modules/myPage.js
+++ sns-frontend/react-server/src/modules/myPage.js
@@ -1,33 +1,40 @@
-// modules/myPage.js
+import { createAction, handleActions } from 'redux-actions';
+import { takeLatest } from 'redux-saga/effects';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
+import * as myPageAPI from '../lib/api/myPage';
 
-// 액션 타입 정의
-const SET_MY_PAGE_DATA = 'myPage/SET_MY_PAGE_DATA';
+const [LIST, LIST_SUCCESS, LIST_FAILURE] =
+    createRequestActionTypes('myPage/LIST');
 
-// 액션 생성자 함수 정의
-export const setMyPageData = (myPageData) => ({
-  type: SET_MY_PAGE_DATA,
-  payload: myPageData,
-});
+export const list = createAction(LIST, (userNo) => (userNo));
 
-// 초기 상태 정의
+const listSaga = createRequestSaga(LIST, myPageAPI.list);
+
+export function* myPageSaga() {
+  yield takeLatest(LIST, listSaga);
+}
+
 const initialState = {
-  myPageData: {
-    visitCount: 0, // 예시: 초기 값으로 0 설정
-    stateMessage: '', // 예시: 초기 값으로 빈 문자열 설정
-    no: null,       // 예시: 초기 값으로 null 설정
-  },
+  myPage: [],
+  userNo: 0,
+  myPageError: null,
 };
 
-// 리듀서 함수 정의
-export default function myPageReducer(state = initialState, action) {
-  switch (action.type) {
-    case SET_MY_PAGE_DATA:
-      return {
+const myPage = handleActions(
+    {
+      [LIST_SUCCESS]: (state, { payload: myPage }) => ({
         ...state,
-        myPageData: action.payload,
-      };
-      // 다른 액션 타입에 대한 처리도 추가할 수 있습니다.
-    default:
-      return state;
-  }
-}
+        myPage,
+        myPageError: null,
+      }),
+      [LIST_FAILURE]: (state, { payload: error }) => ({
+        ...state,
+        myPageError: error,
+      }),
+    },
+    initialState
+);
+
+export default myPage;
