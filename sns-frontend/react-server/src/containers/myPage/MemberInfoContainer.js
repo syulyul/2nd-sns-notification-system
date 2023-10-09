9e44fce17aa2e3eb836e827ef979b138301227e1
tjswns <tjswns99@naver.com>
2023-10-09 12:12:06 +0900

    mypage insert test

--- sns-frontend/react-server/src/containers/myPage/MemberInfoContainer.js
+++ sns-frontend/react-server/src/containers/myPage/MemberInfoContainer.js
@@ -1,6 +1,5 @@
 import {useDispatch, useSelector} from 'react-redux';
 import MemberInfoComponent from '../../components/myPage/MemberInfoComponent';
-import { changeField, initializeForm, info } from '../../modules/myPage';
 import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 const MemberInfoContainer = () => {
@@ -15,25 +14,6 @@ const MemberInfoContainer = () => {
     visitCount: myPage.visitCount,
     stateMessage: myPage.stateMessage,
   }));
-
-  useEffect(() => {
-    info(initializeForm());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (Error) {
-      console.log('오류 발생');
-      console.log(Error);
-      Error('로그인 실패');
-      return;
-    }
-    if (user) {
-      console.log('로그인 성공');
-      console.log(user);
-      // navigate(`/`);
-      navigate(`/myPage/${user.no}`);
-    }
-  }, [user, Error, dispatch]);
 
   return (
       <MemberInfoComponent
--- sns-frontend/react-server/src/modules/myPage.js
+++ sns-frontend/react-server/src/modules/myPage.js
@@ -1,33 +1,58 @@
-// modules/myPage.js
+import { createAction, handleActions } from 'redux-actions';
+import createRequestSaga, {
+  createRequestActionTypes,
+} from '../lib/createRequestSaga';
+import * as myPageAPI from '../lib/api/myPage';
+import { takeLatest } from 'redux-saga/effects';
 
-// 액션 타입 정의
-const SET_MY_PAGE_DATA = 'myPage/SET_MY_PAGE_DATA';
 
-// 액션 생성자 함수 정의
-export const setMyPageData = (myPageData) => ({
-  type: SET_MY_PAGE_DATA,
-  payload: myPageData,
-});
+const CHANGE_FIELD = 'myPage/CHANGE_FIELD';
+const INITIALIZE_FORM = 'myPage/INITIALIZE_FORM';
 
-// 초기 상태 정의
+// 백엔드 서버와 통신하는 경우
+const [INFO, INFO_SUCCESS, INFO_FAILURE] = createRequestActionTypes('myPage/{no}');
+
+// INCREASE 액션 타입을 myPage 네임스페이스 아래에 추가
+
+export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
+  key,
+  value,
+}));
+export const initializeForm = createAction(INITIALIZE_FORM, () => {});
+
+export const info = createAction(
+    INFO,
+    ({ visitCount, no, stateMessage }) => ({
+      visitCount,
+      stateMessage,
+      no
+    })
+);
+
+const infoSaga = createRequestSaga(INFO, myPageAPI.info);
+export function* myPageSaga() {
+  yield takeLatest(INFO, infoSaga);
+}
+
 const initialState = {
-  myPageData: {
-    visitCount: 0, // 예시: 초기 값으로 0 설정
-    stateMessage: '', // 예시: 초기 값으로 빈 문자열 설정
-    no: null,       // 예시: 초기 값으로 null 설정
-  },
+  visitCount: '',
+  stateMessage:'',
+  no: '',
 };
 
-// 리듀서 함수 정의
-export default function myPageReducer(state = initialState, action) {
-  switch (action.type) {
-    case SET_MY_PAGE_DATA:
-      return {
+const myPage = handleActions(
+    {
+      [INFO_SUCCESS]: (state, { payload: page }) => ({
         ...state,
-        myPageData: action.payload,
-      };
-      // 다른 액션 타입에 대한 처리도 추가할 수 있습니다.
-    default:
-      return state;
-  }
-}
+        page,
+      }),
+      [INFO_FAILURE]: (state, { payload: error }) => ({
+        ...state,
+        error,
+      }),
+      // INCREASE 액션 핸들링 추가
+    },
+    initialState
+);
+
+export default myPage;
