da4a05a4ccbd56073f90e65f446ace557c5abf88
syulyul <syul9800@gmail.com>
2023-10-09 21:15:24 +0900

    test : 채팅 backend 개발 중

--- sns-backend/nodejs-server/src/api/index.js
+++ sns-backend/nodejs-server/src/api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import notification from "./notification";
+import chatRoom from "./chatRoom";
 
 const api = express.Router();
 
 api.use("/notificatoin", notification);
+api.use("/chatRoom", chatRoom);
 
 export default api;
