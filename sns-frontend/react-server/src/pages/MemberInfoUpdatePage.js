22dde7e0bf5dfb35d1f6b56adb6dd572371de65d
tjswns <tjswns99@naver.com>
2023-10-17 20:26:44 +0900

    마이페이지에 페이지 수정

--- sns-frontend/react-server/src/pages/MemberInfoUpdatePage.js
+++ sns-frontend/react-server/src/pages/MemberInfoUpdatePage.js
@@ -1,7 +1,17 @@
 import MemberInfoUpdateContainer from '../containers/myPage/MemberInfoUpdateContainer';
+import HeaderContainer from "../containers/common/HeaderContainer";
+import SearchUserContainer from "../containers/myPage/SearchUserContainer";
+import MemberInfoContainer from "../containers/myPage/MemberInfoContainer";
 
 const MemberInfoUpdatePage = () => {
-  return <MemberInfoUpdateContainer />;
+  return    <>
+    <HeaderContainer />
+
+    <MemberInfoContainer />
+
+    <div style={{ marginLeft: '600px', marginBottom: '30px', marginTop: '-20px'}}>
+      <MemberInfoUpdateContainer />
+    </div>  </>
 };
 
 export default MemberInfoUpdatePage;
