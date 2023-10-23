54e069c410653a012dc6a07608d4a5dcf8601bc0
werybalert <hooil0916@gmail.com>
2023-10-23 21:46:49 +0900

    1차 소켓 연결 전

--- sns-backend/nodejs-server/src/main.js
+++ sns-backend/nodejs-server/src/main.js
@@ -9,9 +9,6 @@ import api from './api';
 import mongodbConnect from './schemas';
 import redisConnect from './redis';
 import webSocket from './socket';
-import http from 'http';
-import https from 'https';
-import fs from 'fs';
 
 const {
   PORT,
@@ -22,22 +19,10 @@ const {
   BUILD_DIRECTORY,
 } = process.env;
 
-const HTTP_PORT = 80;
-// const HTTPS_PORT = PORT || 443;
-const HTTPS_PORT = PORT || 3001;
-
-
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/bitsns.site/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/bitsns.site/fullchain.pem')
-};
-
-// app.set('port', PORT);
-
-
 const app = express();
-
+app.set('port', PORT);
 mongodbConnect();
+
 redisConnect();
 
 if (NODE_ENV === 'production') {
@@ -49,19 +34,21 @@ if (NODE_ENV === 'production') {
 const corsOptions = {
   origin: [REACT_SERVER_URL, SPRING_SERVER_URL],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  optionsSuccessStatus: 200,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true,
 };
 app.use(cors(corsOptions));
 
 const buildDirectory = path.resolve(__dirname, BUILD_DIRECTORY);
+
 app.use('/', express.static(buildDirectory));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(COOKIE_SECRET));
 
 app.use('/node', api);
-app.use('/clova', express.static(path.join(__dirname, '../clova')));
+
 app.use('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, BUILD_DIRECTORY + '/index.html'));
 });
@@ -78,14 +65,8 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
 });
 
-// HTTP 서버 생성
-http.createServer(app).listen(HTTP_PORT, () => {
-  console.log(`HTTP Server listening on port ${HTTP_PORT}`);
-});
-
-// HTTPS 서버 생성
-const server = https.createServer(options, app).listen(HTTPS_PORT, () => {
-  console.log(`HTTPS Server listening on port ${HTTPS_PORT}`);
+const server = app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
 
 webSocket(server, app);
